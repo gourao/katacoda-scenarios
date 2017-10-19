2ad54d221e80caa9ecd1fe78c09caee3e7f17c98
gou <gou@portworx.com>
2017-10-19 20:19:05 +0000

    install px

--- deploy-px/courseBase.sh
+++ deploy-px/courseBase.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -xe
 
-@stty -echo
-mknod -m660 /dev/loop18 b 7 18
-touch /var/loop_device_file
-truncate -s 64G /var/loop_device_file
-losetup /dev/loop18 /var/loop_device_file
-stty echo
+mknod -m660 /dev/loop18 b 7 18 &> /dev/null
+touch /var/loop_device_file &> /dev/null
+truncate -s 64G /var/loop_device_file &> /dev/null
+losetup /dev/loop18 /var/loop_device_file &> /dev/null
