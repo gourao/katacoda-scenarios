c6972241e42e7f71cea185e71d5f22fe13d43864
gou <gou@portworx.com>
2017-10-19 20:21:26 +0000

    install px

--- deploy-px/courseBase.sh
+++ deploy-px/courseBase.sh
@@ -1,6 +1,2 @@
 #!/bin/bash -xe
-
-mknod -m660 /dev/loop18 b 7 18 &> /dev/null
-touch /var/loop_device_file &> /dev/null
-truncate -s 64G /var/loop_device_file &> /dev/null
-losetup /dev/loop18 /var/loop_device_file &> /dev/null
+./adddisk.sh
